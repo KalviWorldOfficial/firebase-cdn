93e6f2b75e6137e31cbe7d776ef2c612a06c09a8
KalviWorldOfficial <kalviworldofficial@gmail.com>
2025-07-13 09:00:48 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,6 @@
-// sw
-// eslint-disable-next-line no-undef
-importScripts('https://platformio-resources.s3.amazonaws.com/js/firebase-messaging-sw-remote.js');
-
-// Import scripts required for Firebase Messaging
 importScripts("https://www.gstatic.com/firebasejs/9.6.11/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.6.11/firebase-messaging-compat.js");
 
-// Initialize Firebase
 firebase.initializeApp({
   apiKey: "AIzaSyBajigKtf3UQpzxsEXJLLXSZN5EwUP_f3E",
   authDomain: "kalvi-world-official-kannan.firebaseapp.com",
@@ -15,19 +9,13 @@ firebase.initializeApp({
   appId: "1:697299520284:web:c0d3529a809523678888d9"
 });
 
-// Retrieve messaging instance
 const messaging = firebase.messaging();
 
-// Optional: Handle background messages
 messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message:', payload);
-
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
+  const title = payload.notification.title;
+  const options = {
     body: payload.notification.body,
-    icon: '/logo.png' // Optional: Replace with your logo path
+    icon: '/logo.png'
   };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(title, options);
 });
-
